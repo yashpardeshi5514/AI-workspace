7cac09dfe35894de56d5dfa9e5f845cca3df6b8b
Yash Pardeshi <yashpardeshi9545@gmail.com>
2026-05-24 19:03:43 +0530

    fix: resolve typescript build errors and frontend type issues

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,18 @@
 module.exports = {
+  preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.test.ts', '**/*.spec.ts'],
+  testMatch: ['**/__tests__/**/*.test.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '<rootDir>/tests/'],
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: {
+          module: 'CommonJS',
+        },
+      },
+    ],
+  },
   collectCoverageFrom: [
     'src/**/*.ts',
     '!src/**/*.d.ts',
